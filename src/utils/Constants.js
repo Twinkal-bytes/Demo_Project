5e3174f926e0034beb608515002f50b40c84263f
Twinkal-Bytes <113351581+Twinkal-bytes@users.noreply.github.com>
2022-11-15 18:51:16 +0530

    Update Constants.js

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -33,6 +33,6 @@ export const categories = [
 export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
 export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
 export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery'; //Title Change Here While Coding about this
+export const demoChannelTitle = 'React Js Learning'; //Title Change Here While Coding about this
 export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
